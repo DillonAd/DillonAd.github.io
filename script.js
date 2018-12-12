4972bbb13d161dc3f521d31b0277b4ea61591582
dillon <28228453+DillonAd@users.noreply.github.com>
2018-12-11 21:29:35 -0600

    Switching from ShowdownJS to GitHub's APIs

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-const converter = new showdown.Converter();
 const title = "Automate The Planet!";
 
-async function query(url, callback) {
+async function http(method, url, callback, requestBody) {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = () => callback(xhttp.response);
     xhttp.onerror = (err) => console.error(err);
-    xhttp.open("GET", url, true);
-    await xhttp.send();
+    xhttp.open(method, url, true);
+    await xhttp.send(JSON.stringify(requestBody));
+}
+
+async function query(url, callback, requestBody) {
+    await http("GET", url, callback, requestBody);
+}
+
+async function push(url, callback, requestBody) {
+    await http("POST", url, callback, requestBody);
 }
 
 async function loadContent(name, url) {
-    await query(url, (content) => {
-        const newTitle = title + " - " + name;
-        document.title = newTitle;
-
-        const markdown = converter.makeHtml(content);
-        document.getElementById("content").innerHTML = markdown;
+    await query(url, async (content) => {
+        document.title = title + " - " + name;
+        await push("https://api.github.com/markdown", 
+            (response) => document.getElementById("content").innerHTML = response,
+            { text: content, mode: "markdown", context: "" });
     });
 }
 
@@ -23,12 +29,11 @@ async function start() {
     document.getElementById("header").innerText = title;
 
     await getPostList();
-    await loadContent("Home", "https://raw.githubusercontent.com/DillonAd/DillonAd.github.io/master/Home.md");
+    await loadContent("Home", url + "https://raw.githubusercontent.com/DillonAd/DillonAd.github.io/master/Home.md");
 }
 
 async function getPostList() {
-    const url = "https://api.github.com/repos/DillonAd/DillonAd.github.io/contents/posts";
-    await query(url, populatePostList)
+    await query(url + "https://api.github.com/repos/DillonAd/DillonAd.github.io/contents/posts", populatePostList)
 }
 
 async function populatePostList(response) {
