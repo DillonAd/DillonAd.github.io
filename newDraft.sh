b32814d90f720de4a6bfd89580cf8ed549874853
dillon <28228453+DillonAd@users.noreply.github.com>
2019-03-07 22:52:16 -0600

    Adding Liquid template headers for posts to script

--- newDraft.sh
+++ newDraft.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+title=$1
 now=$(date +'%Y-%m-%d')
-fileName=$now-$1
-touch ./_drafts/$fileName
-chmod 744 ./_drafts/$fileName
+fileName=$now-$title
+filePath=./_drafts/$fileName.md
+touch $filePath
+chmod 744 $filePath
+
+echo --- >> $filePath
+echo layout: post >> $filePath
+echo title: $title >> $filePath
+echo category: [] >> $filePath
+echo tags: [] >> $filePath
+echo date: $now >> $filePath
+echo --- >> $filePath
